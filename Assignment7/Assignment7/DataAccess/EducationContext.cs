e7c16ddab7dc1ff725bb89e8d98475f5a73d7ce9
Keuthonymous <mikie.daughtrey@gmail.com>
2017-06-30 11:57:11 +0200

    Framework with database created

---
+++ Assignment7/Assignment7/DataAccess/EducationContext.cs
@@ -0,0 +1,16 @@
+﻿using Assignment7.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assignment7.DataAccess
+{
+    public class EducationContext : DbContext
+    {
+        public EducationContext() : base("DefaultConnection") { }
+
+        public DbSet<Color> Colors { get; set; }
+    }
+}
---
+++ Assignment7/Assignment7/Models/Color.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment7.Models
+{
+    public class Color
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public ColorType Colors { get; set; }
+
+
+        public enum ColorType
+        { 
+            Red,
+            Orange,
+            Yellow,
+            Green,
+            Blue,
+            Indigo,
+            Violet
+        }
+    }
+}
