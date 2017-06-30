b21ce25f4427e18bad836c08255660daa74d4f94
Tarek Bouzo <tarekbozo@gmail.com>
2017-06-30 13:35:45 +0200

    add Textcontroller

---
+++ Assignment7/Assignment7/Models/Punctuation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment7.Models
+{
+    public class Punctuation
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Text { get; set; }
+    }
+}
